f45c5eb70542522b9cf7f158469b013fc6310daf
Egor <egorchik002@mail.ru>
2022-10-24 11:34:57 +0300

    rename functions, fix linter, delete folder

---
+++ Lesson 2/src/solution.ts
@@ -0,0 +1,27 @@
+export const findOdd = (Arr: number[]) => {
+  const count = (arr: number[], val: number) => arr.filter((item: number) => item === val).length;
+  let result = 0;
+  Arr.forEach((arrnum: number) => {
+    if (count(Arr, arrnum) % 2 !== 0) {
+      result = arrnum;
+    }
+  });
+  return result;
+};
+
+export const revrot = (str: string, sz: number) => {
+  const ln = str.length;
+  if (sz < 1 || !str || sz > ln) {
+    return '';
+  }
+
+  const checkEven = (s: string) => (Array as any).prototype.reduce.call(s, (acc: number, val: number) => acc + Number(val) ** 3, 0) % 2 === 0;
+  const reverse = (s: string) => s.split('').reverse().join('');
+  const rotate = (s: string) => s.slice(1) + s.slice(0, 1);
+
+  const arr = [];
+  for (let i = 0; i < ln; i += sz) {
+    arr.push(i+sz <= ln ? str.slice(i, i+sz) : '');
+  }
+  return arr.map(str => checkEven(str) ? reverse(str) : rotate(str)).join('');
+};
